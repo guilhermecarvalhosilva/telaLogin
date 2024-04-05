1f18b10dadb85e3f0c8ffb1232a35cf26f038ca5
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-04-04 21:58:22 -0300

    Initial commit

---
+++ script/mudarSenha.js
@@ -0,0 +1,13 @@
+ //  Ação para aparecer a senha
+ document.querySelectorAll('.icone-senha')[0].addEventListener('click', () => {
+    if(document.getElementById('txtsenha').value.length > 0) {
+        if(document.getElementById('txtsenha').type == 'text' ) {
+            document.getElementById('txtsenha').type = 'password'
+            document.querySelectorAll('.icone-senha')[0].innerHTML = ` <i class="bi bi-eye-slash-fill icone-item"></i>`
+        } else {
+            document.getElementById('txtsenha').type = 'text'
+            document.querySelectorAll('.icone-senha')[0].innerHTML = `<i class="bi bi-eye-fill icone-item"></i>`
+        }
+    }
+})
+
