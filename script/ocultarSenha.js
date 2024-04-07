17253976b2aca3c5f0fd43c1142013842e4f32c8
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-04-07 00:15:06 -0300

    Atualização dos comentários

--- script/ocultarSenha.js
+++ script/ocultarSenha.js
@@ -2,9 +2,11 @@
  document.querySelectorAll('.icone-senha')[0].addEventListener('click', () => {
     if(document.getElementById('txtsenha').value.length > 0) {
         if(document.getElementById('txtsenha').type == 'text' ) {
+             //  Para ocultar a senha
             document.getElementById('txtsenha').type = 'password'
             document.querySelectorAll('.icone-senha')[0].innerHTML = ` <i class="bi bi-eye-slash-fill icone-item"></i>`
         } else {
+             //  para mostrar a senha
             document.getElementById('txtsenha').type = 'text'
             document.querySelectorAll('.icone-senha')[0].innerHTML = `<i class="bi bi-eye-fill icone-item"></i>`
         }
--- script/validacaoForm.js
+++ script/validacaoForm.js
@@ -1,47 +1,60 @@
-document.querySelector('#verificar').addEventListener('click', () => {
-    if(verificarEmail() && verificarSenha()) {
-        alert('dados enviados')
-        document.querySelectorAll('input')[0].value = ``
-        document.querySelectorAll('input')[1].value = ``
+    // Evento para acionar a validação do formulario
+    document.querySelector('#verificar').addEventListener('click', () => {
+        if(verificarEmail() && verificarSenha()) {
+            document.querySelectorAll('input')[0].value = ``
+            document.querySelectorAll('input')[1].value = ``
+            document.querySelectorAll('.nome-campo')[0].classList.remove('ativo')
+            document.querySelectorAll('.mensagem')[0].classList.add('ativo')
+            setInterval(() => {
+                document.querySelectorAll('.mensagem')[0].classList.remove('ativo')
+            }, 1000*3)
+        }
+
+    
+
+    })
+
+    // Verificar se o e-mail digitado é valido ou não
+    function verificarEmail() {
+        var emailRegex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if(emailRegex.test(document.querySelectorAll('input')[0].value) && document.querySelectorAll('input')[0].value.length > 0){ 
+            // Para quando o campo está vazio ou o e-mail errado
+            document.querySelectorAll('span')[0].classList.remove('ativo')
+            document.querySelectorAll('input')[0].classList.remove('ativo')
+            return true
+        } else{
+            // Para quando o e-mail está certo
+            document.querySelectorAll('span')[0].classList.add('ativo')
+            document.querySelectorAll('input')[0].classList.add('ativo')
+            document.querySelectorAll('input')[0].value = ``
+            document.querySelectorAll('.nome-campo')[0].classList.remove('ativo')
+            return false
+        }
     }
 
-   
+    // Verificar se a senha digitado é valido ou não
+    function verificarSenha() {
+        if(document.querySelectorAll('input')[1].value.length < 6) {
+                // Para quando o campo está vazio ou tem menos que seis caracteres
+            document.querySelectorAll('span')[1].classList.add('ativo')
+            document.querySelectorAll('input')[1].classList.add('ativo')
+            document.querySelectorAll('input')[1].value = ``
+            return false
+        } else {
+            // Para quando a senha está certo
+            document.querySelectorAll('span')[1].classList.remove('ativo')
+            document.querySelectorAll('input')[1].classList.remove('ativo')
+            return true
+        }
+    }
 
-})
 
-
-function verificarEmail() {
-var emailRegex =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-if(emailRegex.test(document.querySelectorAll('input')[0].value) && document.querySelectorAll('input')[0].value.length > 0){ 
-    document.querySelectorAll('span')[0].classList.remove('ativo')
-    document.querySelectorAll('input')[0].classList.remove('ativo')
-    return true
-} else{
-    document.querySelectorAll('span')[0].classList.add('ativo')
-    document.querySelectorAll('input')[0].classList.add('ativo')
-    document.querySelectorAll('input')[0].value = ``
-    return false
-}
-}
-
-function verificarSenha() {
-if(document.querySelectorAll('input')[1].value.length < 6) {
-    document.querySelectorAll('span')[1].classList.add('ativo')
-    document.querySelectorAll('input')[1].classList.add('ativo')
-    document.querySelectorAll('input')[1].value = ``
-    return false
-} else {
-    document.querySelectorAll('span')[1].classList.remove('ativo')
-    document.querySelectorAll('input')[1].classList.remove('ativo')
-    return true
-}
-}
-
-function acaoEmail() {
-if(document.querySelectorAll('input')[0].value.length > 0) {
-    document.querySelectorAll('.nome-campo')[0].classList.add('ativo')
-} else {
-    document.querySelectorAll('.nome-campo')[0].classList.remove('ativo')
-}
-}
+       // Acão para o placeHolder subir no input de e-mail
+    function acaoEmail() {
+        if(document.querySelectorAll('input')[0].value.length > 0) {
+            document.querySelectorAll('.nome-campo')[0].classList.add('ativo')
+        } else {
+            document.querySelectorAll('.nome-campo')[0].classList.remove('ativo')
+        }
+    }
